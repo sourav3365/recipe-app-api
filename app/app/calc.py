e4786fbf171ccd3993e30214240d5120d8180179
Sourav Choudhury <sourav3365@gmail.com>
2023-06-03 17:38:36 +0530

    dcker compose for Postgres DB done, changes were done inside the docker compose ifile

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+"""Calculator function"""
+
+def add(x,y):
+    """Add x and y and return"""
+
+    return x+y
+
+def subtract(x,y):
+    return x-y
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Tese the calc module"""
+
+    def test_add_unmbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numner"""
+        res = calc.subtract(10,15)
+        self.assertEqual(res,-5)    
